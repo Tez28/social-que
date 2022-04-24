bb087e0079a0549078a7ff36c2571916552ef799
Tez28 <courtezcannady@gmail.com>
2022-04-24 11:03:56 -0400

    final server file

--- server.js
+++ server.js
@@ -10,4 +10,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+// mongoose connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-que', {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+});
 
+// logs mongo queries
+mongoose.set('debug', true);
+
+//port listen and confirmation
+app.listen(PORT, () => console.log(`🌍 Connected on localhost: ${PORT}`));
