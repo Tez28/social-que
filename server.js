447981ff5b777edacd8b6124a2da3b22ec0bb812
Tez28 <courtezcannady@gmail.com>
2022-04-24 10:52:13 -0400

    add middleware

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
+// Add dependencies
 const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.eventNames.PORT || 3001;
+
+// Add middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
