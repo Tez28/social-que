e5ea7dd4f66423d2feaa5aa50be92d352e2ad026
Tez28 <courtezcannady@gmail.com>
2022-04-28 11:07:01 -0400

    user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -9,3 +9,21 @@ const {
     addFriend,
     deleteFriend
 } = require('../../controllers/user-controller');
+
+// api route users
+router.route('/')
+.get(getUsers)
+.post(addUser);
+
+//api routes users id
+router.route('/:id')
+.get(getUserByID)
+.put(updateUser)
+.delete(deleteUser);
+
+//api routes friends id
+router.route('/:id/friends/:friendID')
+.post(addFriend)
+.delete(deleteFriend);
+
+module.exports = router;
